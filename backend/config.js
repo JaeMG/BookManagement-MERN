1c0b3d0d64bf10b8c16cdb68d5a4c68444529b54
JaeMG <jaelen.galindo@my.utsa.edu>
2024-08-19 22:35:48 -0500

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://jaemg8:bZpcRqKHe3UiMELp@books-store-mern.la4zf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
